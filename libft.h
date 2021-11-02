f3840fcfbe6137192a824895d87888cb9d117b89
thibaut1304 <thibaut.husser@free.fr>
2021-11-02 21:17:12 +0100

    color and true/false added in header

--- libft.h
+++ libft.h
@@ -18,6 +18,18 @@
 # include <unistd.h>
 # include <fcntl.h>
 
+# define _NC "\033[00m"
+# define _RED "\033[0;31m"
+# define _GREEN "\033[0;32m"
+# define _YELLOW "\033[0;33m"
+# define _BLUE "\033[0;34m"
+# define _PURPLE "\033[0;95m"
+# define _CYAN "\033[0;36m"
+# define _WHITE "\033[0;37m"
+
+# define TRUE 1
+# define FALSE 0
+
 typedef struct s_list
 {
 	void			*content;
